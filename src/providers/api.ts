6732d36558c3c923b14cfb9f94a9bf1dd1a68448
espinhara <gabrielespinhara13@gmail.com>
2024-11-10 12:37:27 -0300

    feat: ✨ creating provider to access api

---
+++ src/providers/api.ts
@@ -0,0 +1,9 @@
+import axios from 'axios'
+
+export const api = axios.create({
+  baseURL: 'http://localhost:5000/api/',
+  headers: {
+    Authorization: `Bearer ${localStorage.getItem('token')}`
+  }
+});
+
