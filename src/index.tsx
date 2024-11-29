275a056c93e5e107e82dce51b56fbc3014c1cb8b
espinhara <gabrielespinhara13@gmail.com>
2024-11-28 22:50:01 -0300

    feat: ✨ adding cart page on router

--- src/index.tsx
+++ src/index.tsx
@@ -6,36 +6,47 @@ import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from './store';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import AdminProduct from './pages/Admin/components/Product';
-import ProductsTable from './pages/Admin/components/ProductsTable';
 import ProductPurchasePage from './components/ProductPurchasePage';
-import SalesTable from './pages/Admin/components/SalesTable';
 import LoginPage from './pages/Login';
 import Register from './pages/Register';
-import RegisterUsers from './pages/Admin/Register/Users';
 import AdminPage from './pages/Admin';
 import NotFound from './pages/NotFound';
+import CartPage from './pages/Cart';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
+// Crie uma instância de QueryClient
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 2, // Número de tentativas em caso de erro
+      refetchOnWindowFocus: false, // Evita refetch automático ao mudar para a aba
+    },
+  },
+});
 const rootElement = document.getElementById('root');
 if (rootElement) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
     <React.StrictMode>
-      <Provider store={store}>
-        <Router>
-          <Routes>
-            <Route path='/' element={
-              <App />
-            } />
-            <Route path='/404' element={<NotFound />} />
-            <Route path='/admin' element={<AdminPage />} />
-            <Route path="/product/:id" element={<ProductPurchasePage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </Router>
-      </Provider>
+      <QueryClientProvider client={queryClient}>
+
+        <Provider store={store}>
+          <Router>
+            <Routes>
+              <Route path='/' element={
+                <App />
+              } />
+              <Route path='/404' element={<NotFound />} />
+              <Route path='/admin' element={<AdminPage />} />
+              <Route path="/product/:id" element={<ProductPurchasePage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/cart" element={<CartPage />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </Router>
+        </Provider>
+      </QueryClientProvider>
     </React.StrictMode>
   );
 }
