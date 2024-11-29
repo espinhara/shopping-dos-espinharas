33f933c2bbf1fe24bb87aea6993c12826328f6dd
espinhara <gabrielespinhara13@gmail.com>
2024-11-28 23:32:25 -0300

    perf: 🚀 updating provider

--- src/providers/api.ts
+++ src/providers/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'http://localhost:5000/api/',
+  baseURL: 'https://shopping-dos-espinharas-backend.onrender.com/api/',
   headers: {
     Authorization: `Bearer ${localStorage?.token}`,
     'Content-Type': 'application/json',
