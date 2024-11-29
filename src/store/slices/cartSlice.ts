9e6416c1e0aa2e4ac1db9803b36286dff752004c
espinhara <gabrielespinhara13@gmail.com>
2024-11-28 22:28:40 -0300

    feat: ✨ update slice

--- src/store/slices/cartSlice.ts
+++ src/store/slices/cartSlice.ts
@@ -1,24 +1,28 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { RootState } from '../index';
+import { CartItem, CartState } from '../../interfaces/cart';
 
-interface CartItem {
-  id: string;
-  name: string;
-  price: number;
-  quantity: number;
-  imageUrl: string;
-}
 
-interface CartState {
-  items: CartItem[];
-  totalAmount: number;
-  totalQuantity: number;
+const loadCartFromStorage = (): CartItem[] => {
+  const cart = localStorage.getItem('cart');
+  return cart ? JSON.parse(cart) as CartItem[] : [];
+};
+const loadTotalAmountFromStorage = (): number => {
+  const cartString = localStorage.getItem('cart');
+  const cart = cartString ? JSON.parse(cartString) as CartItem[] : [];
+  let totalAmount = 0
+  if (cart.length > 0) {
+    cart.forEach((item) => {
+      totalAmount += item.price
+    })
+  }
+  return totalAmount;
 }
 
 const initialState: CartState = {
-  items: [],
-  totalAmount: 0,
-  totalQuantity: 0,
+  items: loadCartFromStorage(),
+  totalAmount: loadTotalAmountFromStorage(),
+  totalQuantity: loadCartFromStorage().length,
 };
 
 const cartSlice = createSlice({
@@ -28,18 +32,35 @@ const cartSlice = createSlice({
     addItemToCart: (state, action: PayloadAction<CartItem>) => {
       const newItem = action.payload;
       const existingItem = state.items.find(item => item.id === newItem.id);
-
-      state.totalQuantity++;
+      if (existingItem && existingItem.quantity + 1 > existingItem.stock) {
+        alert('Quantidade exede o estoque disponível!')
+        return
+      }
+      if (newItem.quantity + 1 > newItem.stock) {
+        alert('Quantidade exede o estoque disponível!')
+        return
+      }
       if (existingItem) {
         existingItem.quantity++;
-        existingItem.price += newItem.price;
+        // existingItem.price += newItem.price;
       } else {
         state.items.push({
           ...newItem,
+          stock: newItem.stock,
           quantity: 1,
         });
       }
+      state.totalQuantity++;
       state.totalAmount += newItem.price;
+
+      localStorage.setItem('cart', JSON.stringify(state.items));
+    },
+    updateItemQuantity: (state, action: PayloadAction<{ id: string; quantity: number }>) => {
+      const item = state.items.find((item) => item.id === action.payload.id);
+      if (item) {
+        item.quantity = action.payload.quantity;
+      }
+      localStorage.setItem('cart', JSON.stringify(state.items));
     },
     removeItemFromCart: (state, action: PayloadAction<string>) => {
       const id = action.payload;
@@ -57,15 +78,26 @@ const cartSlice = createSlice({
         }
       }
     },
+    removeItemsFromCart: (state, action: PayloadAction<string>) => {
+      const id = action.payload;
+      const existingItem = state.items.find(item => item.id === id);
+
+      if (existingItem) {
+        state.totalQuantity -= existingItem.quantity;
+        state.totalAmount -= existingItem.price * existingItem.quantity;
+        state.items = state.items.filter(item => item.id !== id);
+      }
+    },
     clearCart: (state) => {
       state.items = [];
       state.totalAmount = 0;
       state.totalQuantity = 0;
+      localStorage.removeItem('cart');
     },
   },
 });
 
-export const { addItemToCart, removeItemFromCart, clearCart } = cartSlice.actions;
+export const { addItemToCart, updateItemQuantity, removeItemFromCart, removeItemsFromCart, clearCart } = cartSlice.actions;
 
 export const selectCartItems = (state: RootState) => state.cart.items;
 export const selectCartTotalAmount = (state: RootState) => state.cart.totalAmount;
