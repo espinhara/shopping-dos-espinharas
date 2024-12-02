530fe2b9336fdafc2a3d0ccb6fb11f17ee9b7db5
espinhara <gabrielespinhara13@gmail.com>
2024-12-02 00:13:28 -0300

    feat: ✨ adding photo on loading

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,13 +1,32 @@
 import { CircularProgress } from "@mui/material";
+import logo from '../assets/logo.svg'
 import React from "react";
 export const LoadingApp: React.FC = () => {
   return (
-    <CircularProgress sx={{
-      color: "#5B1B64",
-      position: 'fixed', // Fixa o componente em relação à tela inteira
-      top: '50%', // Move o componente 50% para baixo
-      left: '50%', // Move o componente 50% para a direita
-      transform: 'translate(-50%, -50%)', // Ajusta o ponto de ancoragem para o centro
-    }} />);
+    <div
+      style={{
+        position: 'fixed', // Fixa o componente em relação à tela inteira
+        top: '50%', // Move o componente 50% para baixo
+        left: '50%', // Move o componente 50% para a direita
+        transform: 'translate(-50%, -50%)', // Ajusta o ponto de ancoragem para o centro
+        display: 'flex', // Ativa o flexbox
+        flexDirection: 'column', // Alinha os elementos verticalmente
+        alignItems: 'center', // Centraliza os elementos horizontalmente
+      }}
+    >
+      {/* Shopping dos Espinhara's */}
+      <img
+        style={{ height: '200px', marginBottom: '26px' }} // Espaçamento entre imagem e spinner
+        src={logo}
+        alt="Logo"
+      />
+      <CircularProgress
+        sx={{
+          height: "50px",
+          color: '#5B1B64',
+        }}
+      />
+    </div>
+  );
 
 }
