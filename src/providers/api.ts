9206b02449e1888e80c1ff52dbba424565809afd
espinhara <gabrielespinhara13@gmail.com>
2024-12-07 18:50:34 -0300

    feat: ✨ update access api using env

--- src/providers/api.ts
+++ src/providers/api.ts
@@ -1,7 +1,14 @@
 import axios from 'axios'
+const getApiUrl = (): string => {
+  const localApi = process.env.REACT_APP_LOCAL_API as string;
+  const nodeApi = process.env.REACT_APP_HOST_API as string;
+  const isProd = process.env.REACT_APP_NODE_ENV === "production";
+  const baseUrl = isProd ? nodeApi : localApi;
+  return baseUrl;
+};
 
 export const api = axios.create({
-  baseURL: 'https://shopping-dos-espinharas-backend.onrender.com/api/',
+  baseURL: getApiUrl(),
   headers: {
     Authorization: `Bearer ${localStorage?.token}`,
     'Content-Type': 'application/json',
