dec967b4b23043972dd44ce7175c0b8c15e83c65
espinhara <gabrielespinhara13@gmail.com>
2024-12-07 18:58:25 -0300

    feat: ✨ adding search input on product list

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,25 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Grid from '@mui/material/Grid';
 import ProductCard from './ProductCard';
 import { Product } from '../interfaces/product';
+import { Box, } from '@mui/material';
+import SearchInput from './SearchInput';
 
 interface ProductListProps {
   products: Product[];
+  setProducts: (products: Product[]) => void;
+  setLoading: (bool: boolean) => void;
 }
 
-const ProductList: React.FC<ProductListProps> = ({ products }) => {
+const ProductList: React.FC<ProductListProps> = ({ products, setProducts, setLoading }) => {
+  const [searchInput,] = useState<string>('');
   return (
-    <Grid container spacing={4} mb={8}>
-      {products.map((product) => (
-        <Grid item key={product._id} xs={12} sm={6} md={4} lg={3}>
-          <ProductCard
-            id={product._id}
-            name={product.name}
-            price={product.price}
-            imageUrl={product.imageUrls[0]} quantity={product.quantity} />
-        </Grid>
-      ))}
-    </Grid>
+    <Box>
+      <SearchInput data={{
+        searchInput,
+      }} onSearch={function (list: Product[]): void { setProducts(list); }}
+        setLoading={setLoading} />
+      <Grid container spacing={4} mb={8}>
+        {products.map((product) => (
+          <Grid item key={product._id} xs={12} sm={6} md={4} lg={3}>
+            <ProductCard
+              id={product._id}
+              name={product.name}
+              price={product.price}
+              imageUrl={product.imageUrls[0]} quantity={product.quantity} />
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
   );
 };
 
